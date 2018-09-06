7b9fa0708f7f84f52488aa8afb00639d760cc7ff
Michael Hohenstein <michael.hohenstein@gmx.de>
2018-09-06 15:31:17 +0200

    Funktionserweiterung Web App

--- Raumplan/Stand-Alone Web App/script.js
+++ Raumplan/Stand-Alone Web App/script.js
@@ -1,3 +1,18 @@
 window.addEventListener("load", function(){
-    
+
+    var Grundriss = document.getElementById("Grundriss").contentDocument;
+    /*window.setTimeout(function(){
+        location.reload(true);
+    }, 1000)*/
+})
+
+window.addEventListener("load", function(){
+    req = new XMLHttpRequest();
+    req.open("get", "rooms.js", true);
+    req.addEventListener("readystatechange", function(){
+        if(req.target.readyState == 4 && req.target.status == 200){
+            alert(req.target.responseText);
+        }
+    })
+    req.send()
 })
