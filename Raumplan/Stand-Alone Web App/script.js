51940969034185d8359ba68416a8eb87d42e7005
MitchiLaser <michael.hohenstein@gmx.de>
2018-09-06 16:16:01 +0200

    Systemumzug --> Zustandsupdate

--- Raumplan/Stand-Alone Web App/script.js
+++ Raumplan/Stand-Alone Web App/script.js
@@ -1,18 +1,19 @@
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
 
     var Grundriss = document.getElementById("Grundriss").contentDocument;
     /*window.setTimeout(function(){
-        location.reload(true);
+    location.reload(true);
     }, 1000)*/
-})
+});
 
-window.addEventListener("load", function(){
+window.addEventListener("load", function () {
     req = new XMLHttpRequest();
-    req.open("get", "rooms.js", true);
-    req.addEventListener("readystatechange", function(){
-        if(req.target.readyState == 4 && req.target.status == 200){
+    req.open("GET", "rooms.json", true);
+    req.addEventListener('readystatechange', function (req) {
+        //console.log(req.target.status);
+        if (req.target.readyState == 4 && req.target.status == 200) {
             alert(req.target.responseText);
         }
     })
-    req.send()
-})
+    req.send();
+});
